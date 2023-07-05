37ab09ec49c56d99943432cdb85e9b71cfdf50bc
eypklc <ekilic3844@gmail.com>
2023-07-06 01:42:17 +0300

    osmis

---
+++ src/day2/Scan1.java
@@ -0,0 +1,20 @@
+package day2;
+
+import java.util.Scanner;
+
+public class Scan1 {
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+        Scanner scanner =new Scanner(System.in);
+        String userName = scanner.next();
+        System.out.println(userName + " is a nice name.");
+
+        System.out.println("\nHow old are you, " + userName + "?");
+        int userAge = scanner.nextInt();
+        System.out.println("Your name is " + userName + " and you are " + userAge + " years old.");
+
+        System.out.println("\nHow tall are you in meters?");
+        double userHeight = scanner.nextDouble();
+        System.out.println("You are " + userHeight + " meters tall.");
+    }
+}
