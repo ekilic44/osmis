d73917e5358451f62e8dc7dca420005f93e5d2f5
ekilic44 <ekilic3844@gmail.com>
2023-07-08 19:14:35 +0300

    osmis

--- src/day2/Scan1.java
+++ src/day2/Scan1.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 
 public class Scan1 {
     public static void main(String[] args) {
+
+        System.out.println(4.5-2.7);
+        System.out.format("I have %d cats, %d dogs, and 1 %s.", 7, 2, "panther");
+
+        /*Scanner scanner = new Scanner(System.in);
+
         System.out.println("What is your name?");
-        Scanner scanner =new Scanner(System.in);
         String userName = scanner.next();
         System.out.println(userName + " is a nice name.");
 
@@ -15,6 +20,6 @@ public class Scan1 {
 
         System.out.println("\nHow tall are you in meters?");
         double userHeight = scanner.nextDouble();
-        System.out.println("You are " + userHeight + " meters tall.");
+        System.out.println("You are " + userHeight + " meters tall.");*/
     }
 }
